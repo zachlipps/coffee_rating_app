401c0c49713a2d80c6129e14b637a7d8915f5466
Zachary Lipps <zlipps@MacBook-Pro-3.local>
2019-03-20 08:15:21 -0700

    fetch and render list of coffee

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Header from './Components/Header/Header';
+import CoffeeList from './Components/CoffeeList/CoffeeList'
 
 function Home() {
   return <h2>Home</h2>;
@@ -10,6 +11,15 @@ function Home() {
 
 
 class App extends Component {
+  state = {
+    coffeeList: []
+  }
+
+  componentDidMount() {
+    fetch("/API/coffee-sample-data.json")
+      .then(response => response.json())
+      .then(({ coffee }) => this.setState({ coffeeList: coffee }));
+  }
 
   render() {
     return (
@@ -18,6 +28,10 @@ class App extends Component {
           <Header/>
 
           <Route path="/" exact component={Home} />
+          <Route 
+            path="/coffee/"
+            render={(props) => <CoffeeList coffeeList={this.state.coffeeList} />} 
+          />
         </div>
       </Router>
     );
---
+++ src/Components/CoffeeListItem/CoffeeListItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CoffeeListItem = ({ coffee }) => (
+    <div>
+        { coffee.name }
+    </div>
+);
+
+export default CoffeeListItem;
