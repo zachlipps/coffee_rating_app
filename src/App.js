c265dfc3ab7a377c12b0f613b6a89f67cf20fa00
Zachary Lipps <zlipps@MacBook-Pro-3.local>
2019-03-20 08:11:59 -0700

    implement basic react router, component layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Header from './Components/Header/Header';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Header/>
+
+          <Route path="/" exact component={Home} />
+        </div>
+      </Router>
     );
   }
 }
