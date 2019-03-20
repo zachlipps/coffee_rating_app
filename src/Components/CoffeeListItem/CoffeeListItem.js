26113d0600f8f14aeabf01cac025f10cd628e346
Zachary Lipps <zlipps@MacBook-Pro-3.local>
2019-03-20 09:09:06 -0700

    display all values on coffee list item

--- src/Components/CoffeeListItem/CoffeeListItem.js
+++ src/Components/CoffeeListItem/CoffeeListItem.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { CONSTANTS } from '../../Utils/Constants';
 
 const CoffeeListItem = ({ coffee }) => (
     <div>
-        { coffee.name }
+        <img 
+            src={ CONSTANTS['IMAGE-ENDPOINT'] + coffee.info.image }
+            alt=""
+        />
+        <div>
+            { coffee.name }
+        </div>
+        <div>
+            { coffee.info['aromatic-profile'] }
+        </div>
+        <div>
+            Intensity: { coffee.info.intensity }
+        </div>
     </div>
 );
 
