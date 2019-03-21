ee824534c6521e5eeec1f1edea828c1d81e78c24
Zachary Lipps <zlipps@MacBook-Pro-3.local>
2019-03-21 13:52:24 -0700

    basic layout for styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const StyledList = styled.div`
+    justify-content: flex-end;
+    background-color: salmon;
+    display: flex;
+    flex: 1 100%;
+    flex-flow: row wrap;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    display: block;
+    padding: 1em;
+    color: black;
+    &:hover {
+        background: #555;
+    }
+`
 
 const Header = () => (
-    <ul>
-        <li>
-            <Link to="/">Home</Link>
-        </li>
-        <li>
-            <Link to="/coffee/">Coffee List</Link>
-        </li>
-    </ul>
+    <StyledList>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/coffee/">Coffee List</StyledLink>
+    </StyledList>
 );
 
 export default Header;
